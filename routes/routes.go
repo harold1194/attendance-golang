8b9fa8c4eb3390e84216c71338160a887f7487ff
Harold <ilaganharold@gmail.com>
2023-06-18 00:37:11 +0800

    update

--- routes/routes.go
+++ routes/routes.go
@@ -2,13 +2,13 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/harold1194/attendance-golang/res/handlers"
+	"github.com/harold/attendance-golang/res/handlers"
 )
 
 func SetupRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/register", handlers.CreateUser)
-	api.Post("/login", handlers.login)
+	api.Post("/login", handlers.Login)
 	api.Get("/get_users/:id", handlers.GetUserByID)
 	api.Get("/users", handlers.GetUsers)
 }
